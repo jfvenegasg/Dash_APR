d14969a5c35294285df3888bb768adb32bb66deb
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-11-23 12:41:40 -0300

    se agregan modulos del dash

---
+++ Dash_apr.py
@@ -0,0 +1,52 @@
+#from collections import namedtuple
+#import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import streamlit_option_menu as menu
+import os
+import plotly.express as px
+
+
+st.set_page_config(layout="wide")
+
+
+
+with st.sidebar:
+    selected = menu.option_menu("Dash APR", ["Inicio","Ubicaciones","Analisis de beneficiarios"], 
+        icons=['house', 'person-rolodex','person-rolodex'], menu_icon="cast", default_index=0)
+    selected
+
+if selected == "Inicio":
+    st.title("Análisis geográfico para sistemas de agua potable rural en la región de los Ríos")
+
+    #c=st.empty()
+    #c.image("docker_1.png")
+
+if selected == "Ubicaciones":
+    
+            
+       
+    if st.button("Carga"):
+        df = pd.read_csv('trips_austin.csv')
+        col1, col2 = st.columns(2)
+        col1.text("Datos del sistema de bicicletas de la ciudad de Austin")
+        col1.dataframe(df.head(10))
+
+        grouped_data = df.groupby('trip_id')['duration_minutes'].sum().reset_index()
+        grouped_data = grouped_data.sort_values(by='duration_minutes', ascending=False)
+        fig = px.bar(grouped_data, x='trip_id', y='duration_minutes')
+        col2.text("Grafico de la duracion de minutos de los viajes del sistema de bicicleta")
+        col2.plotly_chart(fig)    
+
+        with st.container():
+        
+            porcentajes = grouped_data['duration_minutes']/grouped_data['duration_minutes'].sum()*100
+            grouped_data['percentage'] = porcentajes
+            fig_pie = px.pie(grouped_data, names='trip_id', values='percentage', title='Gráfico de Torta')
+            st.text("Grafico de la duracion de minutos de los viajes del sistema de bicicleta")
+            st.plotly_chart(fig_pie) 
+
+if selected == "Analisis de beneficiarios":
+
+    df1=pd.DataFrame()
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# APR
+# Análisis geográfico para sistemas de agua potable rural en la región de los Ríos
 
-Este es el repositorio del proyecto para la conferencia de Python 2022.Aqui estamos realizando un analisis espacial descriptivo de los sistemas de agua potable rural.
 
-![Image text](https://www.bbva.com/wp-content/uploads/2019/05/A-2205-ODS6_CAS-BBVA-1-1920x1180.jpg)
-
-Acá les comparto el enlace para que puedan acceder al notebook -> https://natalyosses.github.io/APR/
